76791bc4aff0d08e8542e1d0905679fa1055e7d8
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2024-10-07 09:48:19 -0400

    fix: update permisions

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,33 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 const routes = require('./src/api/endPoints');
 const cors = require('cors');
 const connectDB = require('./src/config/db');
 
-
 connectDB();
 
 app.use(express.json());
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-/* const clientUrl = process.env.NODE_ENV === 'production' 
-    ? process.env.PROD_CLIENT_URL 
-    : process.env.DEV_CLIENT_URL; */
-
-const clientUrls = [
-    process.env.DEV_CLIENT_URL,
-    process.env.PROD_CLIENT_URL
+// Lista de URLs permitidas (tanto en desarrollo como en producción)
+const allowedOrigins = [
+    'http://localhost:5173', // URL del frontend en desarrollo
+    'https://proyecto-km-git-main-gaspar200309s-projects.vercel.app' // URL del frontend en producción
 ];
-    
+
 app.use(cors({
-    origin: clientUrls,
+    origin: function (origin, callback) {
+        // Permitir solicitudes sin origen (e.g., Postman) o de orígenes permitidos
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    credentials: true 
+    credentials: true // Permitir el uso de cookies y credenciales
 }));
 
 app.use('/', routes);
